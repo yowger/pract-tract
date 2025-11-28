21c6d2a4559561b377e45a0da775b72b6e39e99d
Ver0018 <rogerpantilyowger@gmail.com>
2025-11-29 04:09:29 +0800

    update

--- src/pages/director/StudentManagementPage.tsx
+++ src/pages/director/StudentManagementPage.tsx
@@ -12,7 +12,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
-import { StudentColumns } from "@/features/director/components/StudentColumns"
+import { StudentColumns } from "@/features/director/components/studentColumns"
 import {
     useStudents,
     // useUpdateStudentsAdvisor,
