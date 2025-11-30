0926b7016c64cc40e1505b433c31393efdc1751a
Ver0018 <rogerpantilyowger@gmail.com>
2025-12-01 01:16:43 +0800

    test 2

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000"
+console.log("🚀 ~ BASE_URL:", BASE_URL)
 
 export const publicApi = axios.create({
     baseURL: BASE_URL,
